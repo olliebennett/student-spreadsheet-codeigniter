a6d7b52af110d85dbbf4f87bfc1eedfad1558820
Ollie Bennett <olliebennett@gmail.com>
2013-08-20 08:25:44 +0100

    Updating admin's "user" view

--- application/views/pages/admin/users_view.php
+++ application/views/pages/admin/users_view.php
@@ -4,9 +4,18 @@
 
 <?php foreach ($users as $user) : ?>
 
-<h3>User <b>#<?php echo $user['user_id']; ?></b>: <?php echo $user['user_name']; ?></h3>
+<h3><b>#<?php echo $user['user_id']; ?></b>: <?php echo $user['user_name']; ?></h3>
 
-<p><a href="http://facebook.com/profile.php?id=<?php echo $user['social_identifier_facebook']; ?>">FB#<?php echo $user['social_identifier_facebook']; ?></a></p>
+<p>HashID: '<b><?php echo hashids_encrypt($user['user_id']); ?></b>'</p>
+
+
+<div class="user_pic_container">
+	<a href="http://facebook.com/profile.php?id=<?php echo $user['social_identifier_facebook']; ?>"><img class="user_pic" src="https://graph.facebook.com/<?php echo $user['social_identifier_facebook']; ?>/picture" height="50" width="50" alt="Profile Pic" title="ID=<?php echo $user['social_identifier_facebook']; ?>" />
+	<span class="user_pic_social_overlay"><img src="<?php echo base_url(); ?>assets/img/fb_16x16.png" width="16" height="16" /></span></a> 
+</div>
+
+
+ <div class="clearfix"></div>
 
 <?php d($user, 'full user details'); ?>
 
